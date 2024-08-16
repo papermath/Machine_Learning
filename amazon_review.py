3ec2cd0485f7310e8768202980ad50b68d76c5d0
papermath <AndyChang943@gmail.com>
2024-08-16 14:27:28 -0500

    get code back

--- amazon_review.py
+++ amazon_review.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-
 reviews = pd.read_csv("amazon_reviews.csv")
 
 reviews.dropna(inplace = True)
