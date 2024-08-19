2be3cd8a1d105cece5320075402f170f71abe108
papermath <AndyChang943@gmail.com>
2024-08-19 11:05:45 -0500

    finish logistic regression and fine tune hyperparameters

--- raisin_classification.py
+++ raisin_classification.py
@@ -1,16 +1,15 @@
+#currently working on branch logistic
 import pandas as pd
 import numpy as np
-from scipy.stats import uniform
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.decomposition import PCA
 
 raisin = pd.read_csv("Raisin_Dataset.csv")
 
-raisin.Class = raisin.Class.map({"Kecimen":0, "Besni": 1})
+raisin.Class = raisin.Class.map({"Kecimen": 0, "Besni": 1})
 
 x = raisin.drop(columns = ["Class"])
 y = raisin.Class
@@ -18,15 +17,11 @@ y = raisin.Class
 scaler = StandardScaler()
 scaled_x = scaler.fit_transform(x)
 
-x_train, x_test, y_train, y_test = train_test_split(scaled_x, y, train_size = 0.8, random_state = 69)
+x_train, x_test, y_train, y_test = train_test_split(scaled_x, y, train_size = 0.8, random_state = 42)
 
-svm = SVC(kernel = "rbf", C = 1, gamma = 0.01, random_state = 69)
-svm.fit(x_train, y_train)
-#GridSearchCV outputted C = 1 and gamma = 0.01 as best values with train score of 0.8819444444444444 and test score of 0.8166666666666667
-#RandomSearchCV outputted C = 1 and gamma = 0.07422867291585378 as best values with train score of 0.8847222222222222 and test score of 0.8277777777777777
-#PCA decreased accuracy by around half
-#but due to how close they are I will just let C = 1 and gamma = 0.01 for simplicity
+lr = LogisticRegression(random_state = 42, C = 0.01, tol = 0.00001, max_iter = 10000, solver = "newton-cholesky")
 
-print(svm.score(x_test, y_test))
+lr.fit(x_train, y_train)
 
-
+print(lr.score(x_train, y_train))
+print(lr.score(x_test, y_test))
