041ae83bce0b09d2177f8c76f846f6a99626f2c5
papermath <AndyChang943@gmail.com>
2024-08-18 22:30:22 -0500

    finish svm classifcation for raisins

--- raisin_classification.py
+++ raisin_classification.py
@@ -1,39 +1,32 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import uniform
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.decomposition import PCA
 from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.decomposition import PCA
 
 raisin = pd.read_csv("Raisin_Dataset.csv")
 
-raisin.Class = raisin.Class.map({"Kecimen": 0, "Besni": 1})
+raisin.Class = raisin.Class.map({"Kecimen":0, "Besni": 1})
 
 x = raisin.drop(columns = ["Class"])
 y = raisin.Class
 
 scaler = StandardScaler()
-x = pd.DataFrame(scaler.fit_transform(x), columns = x.columns)
+scaled_x = scaler.fit_transform(x)
 
-for i in [4,5,6]:
-    pca = PCA(n_components = i)
-    x = pca.fit_transform(x)
+x_train, x_test, y_train, y_test = train_test_split(scaled_x, y, train_size = 0.8, random_state = 69)
 
-    x_train, x_test, y_train, y_test = train_test_split(x,y, train_size = 0.8, random_state = 100)
+svm = SVC(kernel = "rbf", C = 1, gamma = 0.01, random_state = 69)
+svm.fit(x_train, y_train)
+#GridSearchCV outputted C = 1 and gamma = 0.01 as best values with train score of 0.8819444444444444 and test score of 0.8166666666666667
+#RandomSearchCV outputted C = 1 and gamma = 0.07422867291585378 as best values with train score of 0.8847222222222222 and test score of 0.8277777777777777
+#PCA decreased accuracy by around half
+#but due to how close they are I will just let C = 1 and gamma = 0.01 for simplicity
 
-    svm = SVC(kernel = "rbf", random_state = 100)
-
-    parameters = {"gamma": np.logspace(-20,-1,40), "C": np.logspace(-20,-1,40)}
-
-    grid = GridSearchCV(estimator = svm, param_grid = parameters, cv = 5)
-    grid.fit(x_train, y_train)
-
-    print(i)
-    print(grid.best_estimator_)
-    print(grid.score(x_test,y_test))
-
-#SVC(C=0.032570206556597696, gamma=0.1, random_state=100)
-#0.8555555555555555
+print(svm.score(x_test, y_test))
 
 
