786ba58b935f5a5f5fc69c4592c578196aaa6de5
papermath <AndyChang943@gmail.com>
2024-08-20 19:10:57 -0500

    include stacking classifier which combines previous models together

--- pumpkin_classification.py
+++ pumpkin_classification.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, StackingClassifier
 from sklearn.model_selection import GridSearchCV
 
 pumpkin = pd.read_csv("Pumpkin_Seeds_Dataset.csv")
@@ -14,6 +15,12 @@ x = pumpkin.drop(columns = ["Class"])
 y = pumpkin.Class
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, train_size = 0.8, random_state = 69)
+
+kfold = StratifiedKFold(n_splits = 3, shuffle = True, random_state = 69)
+stack = StackingClassifier(estimators = [("tree", DecisionTreeClassifier(max_depth = 1)),("forest", RandomForestClassifier(max_depth = 7, max_features = 8)), ("logistic",LogisticRegression(solver = "liblinear", C = 1, penalty = "l2"))], final_estimator = GradientBoostingClassifier(n_estimators = 100), passthrough = True, cv = kfold, stack_method = "predict_proba")
+stack.fit(x_train, y_train)
+print("Stacked Logistic Regression, Random Forest, Adaptive Boost, and Gradient Boosting Classifier Score:")
+print(stack.score(x_test, y_test))
 
 lr = LogisticRegression(solver = "liblinear", C = 1, penalty = "l2")
 lr.fit(x_train, y_train)
